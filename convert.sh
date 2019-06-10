b4138e0d5f1e7d547bbf0f130de5b221930e8075
SeongJae Park <sj38.park@gmail.com>
2019-06-10 10:12:01 +0900

    convert: Enhance readability
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- convert.sh
+++ convert.sh
@@ -10,5 +10,5 @@ REPO_PATH=$1
 START_VER=$2
 END_VER=$3
 
-git -C $REPO_PATH log --pretty=format:user:%aN%n%ct --reverse --raw --encoding=UTF-8 \
-	--no-renames $START_VER..$END_VER
+git -C $REPO_PATH log --pretty=format:user:%aN%n%ct --reverse --raw \
+	--encoding=UTF-8 --no-renames $START_VER..$END_VER
